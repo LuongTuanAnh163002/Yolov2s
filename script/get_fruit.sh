e443a614349cbc0ee86aaf15ae4489ab0ed91cf9
Lương Tuấn Anh <114805429+LuongTuanAnh163002@users.noreply.github.com>
2023-11-16 23:13:09 +0700

    Update get_fruit.sh 1

--- script/get_fruit.sh
+++ script/get_fruit.sh
@@ -8,6 +8,5 @@ url=https://drive.google.com/file/d/1btZfd9hFpY7J_UGDMHkUtia-2VggcLRP/view?usp=s
 f='fruit_dataset.zip' # or 'coco2017labels.zip', 68 MB
 echo 'Downloading' $url$f ' ...'
 curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
-done
 wait # finish background tasks
  
