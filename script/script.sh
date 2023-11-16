bb44b404c16d0807ff8f1f560ecbc613c10c4c9e
Lương Tuấn Anh <114805429+LuongTuanAnh163002@users.noreply.github.com>
2023-11-16 23:06:35 +0700

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -1,14 +1,13 @@
----------------For training---------------------
+#!/bin/bash
+# Soccer player dataset http://cocodataset.org
+# Download command: bash ./scripts/get_fruit.sh
 
-python train.py --data data --epochs --device --data_format
-
----------------For training---------------------
-
-
-
----------------For detect-----------------------
-
-python detect.py --source  --weight --conf_thres --device
-
----------------For detect-----------------------
+# Download/unzip dataset
+d='./' # unzip directory
+url=https://drive.google.com/file/d/1btZfd9hFpY7J_UGDMHkUtia-2VggcLRP/view?usp=sharing/
+f='fruit_dataset.zip' # or 'coco2017labels.zip', 68 MB
+echo 'Downloading' $url$f ' ...'
+curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
+done
+wait # finish background tasks
  
